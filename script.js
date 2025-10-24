a264c94c21c92d374b140e81787a797994565093
nazik-ops <chargynbaevanazik@gmail.com>
2025-10-24 15:26:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let grade=prompt("1ден 5 чейин баалаңыз!")
+switch (grade) {
+    case "1":
+        alert("Эң төмөн")
+        break;
+    case "2":
+        alert("Төмөн")
+        break;
+    case"3":
+    alert("Канааттандырарлык")
+    break;
+    case "4":
+    alert("Жакшы")
+    break;
+    case "5":
+        alert("Эң мыкты!")
+    default:
+        alert(" 1-5 чейин туура баала!")
+        break;
+}
